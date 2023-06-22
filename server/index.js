fcadb9c1569afe2483c814c48f02645326f9ad7f
taalhaataahir0102 <23100293@lums.edu.pk>
2023-06-22 19:18:57 +0500

    community

--- server/index.js
+++ server/index.js
@@ -42,8 +42,28 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-
 const User = mongoose.model('User', userSchema);
+
+const communitySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 15,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 60,
+  },
+  comments: [{
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    comment: String,
+  }],
+});
+const Community = mongoose.model('Community', communitySchema);
 
 // Middleware to authenticate the user
 const authenticateToken = (req, res, next) => {
@@ -148,3 +168,118 @@ app.post('/api/signincheck', async (req, res) => {
       return res.status(500).json({ message: 'Internal server error' });
     }
   });
+
+  app.post('/api/createcommunity', async (req, res) => {
+    const { name, description } = req.body;
+    console.log(name, description);
+  
+    // Validate the community name and description length
+    if (name.trim().length > 15 || description.trim().length > 60) {
+      return res.status(400).json({ msg: 'Invalid community name or description length' });
+    }
+  
+    try {
+      // Create a new community document
+      const newCommunity = new Community({
+        name: name.trim(),
+        description: description.trim(),
+      });
+  
+      // Save the new community document
+      await newCommunity.save();
+  
+      return res.status(201).json({ msg: 'Community created successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ msg: 'Failed to create community' });
+    }
+  });
+
+
+  app.get('/api/communities', async (req, res) => {
+    try {
+      // Retrieve all communities from the table
+      const communities = await Community.find({}, 'name description').exec();
+      return res.status(200).json(communities);
+    } catch (error) {
+      return res.status(500).json({ msg: 'Failed to retrieve communities' });
+    }
+  });
+
+
+  app.post('/api/chat', async (req, res) => {
+    const { community_id, user_id, message } = req.body;
+    console.log(community_id,user_id,message)
+  
+    try {
+      // Find the community based on the community_id
+      const community = await Community.findById(community_id);
+  
+      if (!community) {
+        return res.status(404).json({ msg: 'Community not found' });
+      }
+  
+      // Add the new comment to the community's comments array
+      community.comments.push({ user_id, comment: message });
+      // Save the updated community
+      await community.save();
+  
+      return res.status(200).json({ msg: 'Comment added successfully' });
+    } catch (error) {
+      console.error('Error adding comment:', error);
+      return res.status(500).json({ msg: 'Failed to add comment' });
+    }
+  });
+
+  // Assuming you have the necessary imports and setup for your Express app and Mongoose
+
+  app.post('/api/communitychat', async (req, res) => {
+    const { communityId } = req.body;
+    console.log(communityId);
+  
+    try {
+      // Find the community by its ID and populate the 'comments.user_id' field
+      const community = await Community.findById(communityId).populate('comments.user_id');
+      console.log(community);
+  
+      if (!community) {
+        return res.status(404).json({ error: 'Community not found' });
+      }
+  
+      // Extract the usernames and comments from the community's comments list
+      const chatData = community.comments.map((comment) => {
+        const { user_id, comment: commentText } = comment;
+        return {
+          name: user_id.name,
+          comment: commentText,
+          user_id: user_id._id, // Include the user_id
+        };
+      });
+      console.log(chatData);
+  
+      res.json(chatData);
+    } catch (error) {
+      console.error('Error retrieving community chat:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+
+app.post('/api/getUserName', async (req, res) => {
+  try {
+    const { user_id } = req.body;
+    
+    // Find the user with the given user_id
+    const user = await User.findById(user_id);
+    
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    
+    // Return the user's name
+    res.json({ name: user.name });
+  } catch (error) {
+    console.error('Error fetching user name:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
