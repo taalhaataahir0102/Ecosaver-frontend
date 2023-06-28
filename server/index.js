2312144656d9286fc5d43c8a8c2272e28e1f2fd0
taalhaataahir0102 <23100293@lums.edu.pk>
2023-06-29 02:12:41 +0500

    cc

--- server/index.js
+++ server/index.js
@@ -41,6 +41,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  level: {
+    type: Number,
+  },
+  score: {
+    type: Number,
+  },
+  image: {
+    type: String,
+  },
 });
 const User = mongoose.model('User', userSchema);
 
@@ -54,6 +63,11 @@ const communitySchema = new mongoose.Schema({
     type: String,
     required: true,
     maxlength: 60,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
   comments: [{
     user_id: {
@@ -69,6 +83,7 @@ const Community = mongoose.model('Community', communitySchema);
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
   
     if (token == null) {
       return res.sendStatus(401);
@@ -78,7 +93,8 @@ const authenticateToken = (req, res, next) => {
       if (err) {
         return res.sendStatus(403);
       }
-  
+      console.log(user);
+      console.log(req.params.userID);
       // Check if the token matches the user's ID
       if (user.userID !== req.params.userID) {
         return res.sendStatus(401);
@@ -105,7 +121,7 @@ app.post('/api/createuser', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create a new user
-    const newUser = new User({ name, email, password: hashedPassword });
+    const newUser = new User({ name, email, password: hashedPassword, level:6, score:80, image: null});
     await newUser.save();
 
     return res.status(201).json({ message: 'User created successfully' });
@@ -159,8 +175,10 @@ app.post('/api/signincheck', async (req, res) => {
       const userData = {
         name: user.name,
         email: user.email,
+        level: user.level,
+        score: user.score,
+        image: user.image,
       };
-    
       // Send the user data as the response
       return res.status(200).json({ user: userData });
     } catch (error) {
@@ -170,7 +188,7 @@ app.post('/api/signincheck', async (req, res) => {
   });
 
   app.post('/api/createcommunity', async (req, res) => {
-    const { name, description } = req.body;
+    const { name, description, user_id } = req.body;
   
     // Validate the community name and description length
     if (name.trim().length > 15 || description.trim().length > 60) {
@@ -182,6 +200,7 @@ app.post('/api/signincheck', async (req, res) => {
       const newCommunity = new Community({
         name: name.trim(),
         description: description.trim(),
+        owner: user_id, // Save the user_id of the owner
       });
   
       // Save the new community document
@@ -198,7 +217,7 @@ app.post('/api/signincheck', async (req, res) => {
   app.get('/api/communities/:userID', authenticateToken, async (req, res) => {
     try {
       // Retrieve all communities from the table
-      const communities = await Community.find({}, 'name description').exec();
+      const communities = await Community.find({}, 'name description owner').exec();
       return res.status(200).json(communities);
     } catch (error) {
       return res.status(500).json({ msg: 'Failed to retrieve communities' });
@@ -317,5 +336,97 @@ app.get('/api/viewtheuser/:userID', async (req, res) => {
   } catch (error) {
     console.error('Error fetching user data:', error);
     return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.post('/api/image', async (req, res) => {
+  const { image, userID } = req.body;
+
+  try {
+    // Find the user by ID
+    const user = await User.findById(userID);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update the user's image
+    user.image = image;
+
+    // Save the updated user
+    await user.save();
+
+    return res.json({ message: 'Image updated successfully' });
+  } catch (error) {
+    console.error('Error updating image:', error);
+    return res.status(500).json({ message: 'An error occurred while updating image' });
+  }
+});
+
+app.get('/api/leaderboard', async (req, res) => {
+  try {
+    const users = await User.find()
+      .sort({ level: -1, score: -1 }) // Sort by level (descending) and score (descending)
+      .select('name score level');
+
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching leaderboard:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+
+function authenticateTokenNew(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token === null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
+
+app.get('/api/user', authenticateTokenNew, (req, res) => {
+  // Retrieve the user ID from the authenticated request
+  const userId = req.user.userID;
+  // Use the user ID to retrieve user information from the database
+  User.findById(userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      // Return the user information
+      return res.status(200).json({ user });
+    })
+    .catch(error => {
+      // console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+app.delete('/api/deletecommunity/:userID', authenticateToken, async (req, res) => {
+  const communityId = req.body.communityid;
+
+  try {
+    // Delete the community from the database using the community ID
+    const deletedCommunity = await Community.findByIdAndRemove(communityId);
+
+    if (!deletedCommunity) {
+      return res.status(404).json({ message: 'Community not found' });
+    }
+
+    res.status(200).json({ message: 'Community deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting community:', error);
+    res.status(500).json({ message: 'Failed to delete community' });
   }
 });
